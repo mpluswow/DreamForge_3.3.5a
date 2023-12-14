bc35bc1d174799bc078eaec01d76bf30b8543ca4
unknown <amm.kulik@gmail.com>
2023-12-13 21:21:14 -0800

    Public Repository

---
+++ AzerothSource/data/sql/custom/db_world/2023_11_21_ADDED_RACES_TO_QUEST_TEMPLATE.sql
@@ -0,0 +1,46 @@
+UPDATE`quest_template`SET`AllowableRaces`='0' WHERE id = 9495
+or id = 9492
+or id = 9474
+or id = 9446
+or id = 9494; 
+
+UPDATE`quest_template`SET`AllowableRaces`='886785'WHERE`AllowableRaces`='1'; 
+UPDATE`quest_template`SET`AllowableRaces`='886788'WHERE`AllowableRaces`='4'; 
+
+
+UPDATE`quest_template`SET`AllowableRaces`='886789'WHERE`AllowableRaces`='5'; 
+
+
+UPDATE`quest_template`SET`AllowableRaces`='886792'WHERE`AllowableRaces`='8'; 
+UPDATE`quest_template`SET`AllowableRaces`='886848'WHERE`AllowableRaces`='64'; 
+
+UPDATE`quest_template`SET`AllowableRaces`='886852'WHERE`AllowableRaces`='68'; 
+UPDATE`quest_template`SET`AllowableRaces`='886861'WHERE`AllowableRaces`='77'; 
+
+UPDATE`quest_template`SET`AllowableRaces`='887808'WHERE`AllowableRaces`='1024'; 
+UPDATE`quest_template`SET`AllowableRaces`='887812'WHERE`AllowableRaces`='1028'; 
+
+
+
+UPDATE`quest_template`SET`AllowableRaces`='887813'WHERE`AllowableRaces`='1029'; 
+UPDATE`quest_template`SET`AllowableRaces`='887885'WHERE`AllowableRaces`='1101'; 
+
+UPDATE`quest_template`SET`AllowableRaces`='1077506'WHERE`AllowableRaces`='2'; 
+UPDATE`quest_template`SET`AllowableRaces`='1077520'WHERE`AllowableRaces`='16'; 
+
+UPDATE`quest_template`SET`AllowableRaces`='1077536'WHERE`AllowableRaces`='32'; 
+UPDATE`quest_template`SET`AllowableRaces`='1077632'WHERE`AllowableRaces`='128'; 
+
+UPDATE`quest_template`SET`AllowableRaces`='1077634'WHERE`AllowableRaces`='130'; 
+UPDATE`quest_template`SET`AllowableRaces`='1077648'WHERE`AllowableRaces`='144'; 
+
+UPDATE`quest_template`SET`AllowableRaces`='1077680'WHERE`AllowableRaces`='176'; 
+UPDATE`quest_template`SET`AllowableRaces`='1077682'WHERE`AllowableRaces`='178'; 
+
+UPDATE`quest_template`SET`AllowableRaces`='1078016'WHERE`AllowableRaces`='512'; 
+UPDATE`quest_template`SET`AllowableRaces`='1078018'WHERE`AllowableRaces`='514'; 
+
+UPDATE`quest_template`SET`AllowableRaces`='1078178'WHERE`AllowableRaces`='674'; 
+UPDATE`quest_template`SET`AllowableRaces`='1078194'WHERE`AllowableRaces`='690'; 
+
+UPDATE`quest_template`SET`AllowableRaces`='2097151'WHERE`AllowableRaces`='1791'; 
